9395ba78661659bedb729b90ffa2024141e68882
zhoudm5 <zhoudm5@sany.com.cn>
2021-02-15 20:35:14 +0800

    hooks实现JSX语法

--- src/index.jsx
+++ src/index.jsx
@@ -8,14 +8,29 @@ function render(el, parentElement) {
 	parentElement.appendChild(el)
 }
 
+// 自制hooks函数
+function createHooks(fn, props, children) {
+	props = props || Object.create(null);
+	props.children = children;
+	return fn(props);
+}
+
 // 自制createElement
 function createElement(tagName, attrs, ...children) {
-	const el = document.createElement(tagName)
+	const el = typeof tagName === 'function' ? createHooks(tagName, attrs, children) : document.createElement(tagName)
 	for (const k in attrs) {
 		el.setAttribute(k, attrs[k]);
 	}
 	el.append.apply(el, children);
 	return el;
+}
+
+function App() {
+	return <HelloWorld id="slogen" />
+}
+
+function HelloWorld(props) {
+	return <div id={props.id}>toy react</div>
 }
 
 export const ReactDOM = {
@@ -26,4 +41,4 @@ export const React = {
 	createElement
 };
 
-ReactDOM.render(<div id="slogen">toy react</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
